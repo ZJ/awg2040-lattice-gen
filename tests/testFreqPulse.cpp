79fcf38f7cf25bc7cb8f4740d2af56809f4d8f6f
Zach Smith <zsmith12@umd.edu>
2015-08-20 10:36:16 -0400

    Add tests of equality operator

--- tests/testFreqPulse.cpp
+++ tests/testFreqPulse.cpp
@@ -21,6 +21,27 @@ TEST(FreqPulseExplicitConstructor, DefaultOptionalValues) {
 	EXPECT_FALSE(testPulse.getMarkDuration());
 }
 
+TEST(FreqPulseEquality, SelfEqual) {
+	freqPulse testPulse;
+	EXPECT_TRUE(testPulse==testPulse);
+}
+
+TEST(freqPulseEquality, DiffNotEqual) {
+	freqPulse testPulse, notTestPulse;
+
+	notTestPulse = freqPulse(2,1.0,0,false,false);
+	EXPECT_FALSE(testPulse==notTestPulse) << " Missed " << "frequency.";
+	notTestPulse = freqPulse(0,0.5,0,false,false);
+	EXPECT_FALSE(testPulse==notTestPulse) << " Missed " << "amplitude.";
+	notTestPulse = freqPulse(0,1.0,1,false,false);
+	EXPECT_FALSE(testPulse==notTestPulse) << " Missed " << "duration.";
+	notTestPulse = freqPulse(0,1.0,0,true,false);
+	EXPECT_FALSE(testPulse==notTestPulse) << " Missed " << "start marker.";
+	notTestPulse = freqPulse(0,1.0,0,false,true);
+	EXPECT_FALSE(testPulse==notTestPulse) << " Missed " << "duration marker.";
+
+}
+
 int main(int argc, char * argv[] ) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
