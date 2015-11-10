68dcdce375c6a55e359a0ff7d8baa6d943d049cb
Zach Smith <zsmith12@umd.edu>
2015-11-10 18:50:08 -0500

    Add test for programmingString() method of awg library
    Should probably add a startPulseLength member to do this right.

--- tests/testAWG.cpp
+++ tests/testAWG.cpp
@@ -16,6 +16,18 @@ TEST(AwgClass, ComPort) {
 	EXPECT_EQ(std::string("com1"),testAwg.comPort());
 }
 
+#define S(s) s, sizeof s - 1
+
+TEST(AwgClass, ProgrammingString) {
+	freqPulse	testPulse(100, 0.0, 5.0, 0, true);
+	pulseTrain	testTrain(0.0);
+	testTrain.pushPulse(testPulse);
+
+	awg testAwg(testTrain, 19200, 1000.0, "COM42", "testName.txt");
+	const std::string expectedProgramming(S("DATA:DESTINATION \"testName.txt\"\nDATA:WIDTH 1\nCURVE #15\x7F\x7F\x7F\x7F\x7F\nMARKER:DATA #15\x2\0\0\0\0\nCLOCK:FREQUENCY 1000.000000MHz\nCH1:WAVEFORM \"testName.txt\"\n"));
+	EXPECT_EQ(expectedProgramming, testAwg.programmingString());
+}
+
 int main(int argc, char * argv[] ) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
