471ea4e94c0bb86dfb7ef6e41db12f2b5359bdd2
Zach Smith <zsmith12@umd.edu>
2015-11-23 15:47:39 -0500

    AWG wants sample in multiples of 32
    So pad it out to those multiples.  Fixes problems exposed in prior commit

--- src/awg.cpp
+++ src/awg.cpp
@@ -10,6 +10,20 @@ std::string awg::programmingString() {
 	unsigned long numPoints = outputPulses.numPoints(_sampleRate, true);
 	std::string curveString = outputPulses.waveChars(_sampleRate, numPoints, true);
 	std::string markerString = outputPulses.markerChars(_sampleRate, numPoints, true, round(_startPulseLength/_sampleRate));
+
+	// Figure out how many times to double the length
+	unsigned long paddedPoints;
+	for(paddedPoints=numPoints; (paddedPoints % 32) > 0; paddedPoints = (paddedPoints << 1));
+	curveString.reserve(paddedPoints);
+	markerString.reserve(paddedPoints);
+	// Double it that many times
+	while(paddedPoints != curveString.length()) {
+		curveString += curveString;
+		markerString += markerString;
+	}
+	numPoints = paddedPoints;
+
+	// Grab the actual output lengths
 	char buffer[128];
 	unsigned int charLength = sprintf(buffer, "%lu", numPoints);
 	sprintf(buffer, "#%u%lu", charLength, numPoints);
